87a6ecd26401384a2eba783f982d4e3e8d0af4d3
abhi1302-js <69789359+abhi1302-js@users.noreply.github.com>
2020-10-07 06:55:23 +0400

    commit

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-var Name = document.getElementsByClassName("nam").value;
-var grade = document.querySelector(".class").value;
-var section = document.querySelector(".section").value;
-var agreement = ("I "+ Name +" of "+ grade + " " + section + " agree's to the terms and conditions of this website.")
-function test() {
-   document.querySelector("#agreement").textContent = agreement;
-    
-};
+ function prompt() {
+    alert("I AGREE TO ALL THE TERMS AND CONDITIONS OF THIS WEBSITE");
+    location.replace("mainpage.html");
+   
+}
